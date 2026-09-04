94bdc132ad31021769b7eb7f9064ed793443712b
Ravi Mahadik <drbatrasbackupsync.com>
2026-09-04 17:52:22 +0530

    Fix Azure backend configuration, add provider.tf files, and add -reconfigure to pipeline

--- environments/preprod/backend.tf
+++ environments/preprod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-ravi"
-    storage_account_name = "storagetechaccount"
+    storage_account_name = "storagetechaccounts"
     container_name       = "tfstate"
     key                  = "preprod.terraform.tfstate"
   }
